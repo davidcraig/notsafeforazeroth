4a9a358dac95f3ddf4a1736b673f27ddc89e0db9
davidcraig <davidcraig-uk@protonmail.com>
2024-07-11 18:13:58 +0100

    Update recruitment to a text box until we get class specific.

--- Components/Widgets/Recruitment.js
+++ Components/Widgets/Recruitment.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Card } from '@davidcraig/react-bulma'
 
-const classStatuses = [
-  { name: 'Priest',       status: 'Open', css: 'fg-priest' },
-  { name: 'Mage',         status: 'Open', css: 'fg-mage' },
-  { name: 'Warlock',      status: 'Open', css: 'fg-warlock' },
-  { name: 'Druid',        status: 'Open', css: 'fg-druid' },
-  { name: 'Rogue',        status: 'Open', css: 'fg-rogue' },
-  { name: 'Demon Hunter', status: 'Open', css: 'fg-demonhunter' },
-  { name: 'Monk',         status: 'Open', css: 'fg-monk' },
-  { name: 'Hunter',       status: 'Open', css: 'fg-hunter' },
-  { name: 'Shaman',       status: 'Open', css: 'fg-shaman' },
-  { name: 'Evoker',       status: 'Open', css: 'fg-evoker' },
-  { name: 'Paladin',      status: 'Open', css: 'fg-paladin' },
-  { name: 'Warrior',      status: 'Open', css: 'fg-warrior' },
-  { name: 'Death Knight', status: 'Open', css: 'fg-deathknight' },
-]
+// const classStatuses = [
+//   { name: 'Priest',       status: 'Open', css: 'fg-priest' },
+//   { name: 'Mage',         status: 'Open', css: 'fg-mage' },
+//   { name: 'Warlock',      status: 'Open', css: 'fg-warlock' },
+//   { name: 'Druid',        status: 'Open', css: 'fg-druid' },
+//   { name: 'Rogue',        status: 'Open', css: 'fg-rogue' },
+//   { name: 'Demon Hunter', status: 'Open', css: 'fg-demonhunter' },
+//   { name: 'Monk',         status: 'Open', css: 'fg-monk' },
+//   { name: 'Hunter',       status: 'Open', css: 'fg-hunter' },
+//   { name: 'Shaman',       status: 'Open', css: 'fg-shaman' },
+//   { name: 'Evoker',       status: 'Open', css: 'fg-evoker' },
+//   { name: 'Paladin',      status: 'Open', css: 'fg-paladin' },
+//   { name: 'Warrior',      status: 'Open', css: 'fg-warrior' },
+//   { name: 'Death Knight', status: 'Open', css: 'fg-deathknight' },
+// ]
 
 function getStatusClass (classStatus) {
   const status = classStatus.status
@@ -35,12 +35,15 @@ export default function Recruitment() {
     <Card title='Recruitment' className='widget widget-recruitment'>
       <table className='table is-narrow is-striped'>
         <tbody>
-          {classStatuses.map(s => {
+          {/* {classStatuses.map(s => {
             return <tr key={s.css}>
               <td className={`${s.css} px-2 py-1`}>{s.name}</td>
               <td className={`${getStatusClass(s)} px-2 py-1`}>{s.status}</td>
             </tr>
-          })}
+          })} */}
+          <tr>
+            <td className='recruitment-open px-2 py-1'>We are currently looking for several players, any spec and role are welcome while we build our core team.</td>
+          </tr>
         </tbody>
       </table>
     </Card>
