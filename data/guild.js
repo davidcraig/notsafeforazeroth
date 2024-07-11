2837bea4d861a937a601ea41303901a0d1f8c611
davidcraig <davidcraig-uk@protonmail.com>
2024-07-11 11:07:10 +0100

    Add guild information.

---
+++ data/guild.js
@@ -0,0 +1,12 @@
+const guild = {
+  name: 'Not Safe for Azeroth',
+  server: 'Tarren Mill',
+
+  timeline: [
+    { year: 2024, month: 6, day: 11, date: '2024-06-11', title: 'Discord setup' },
+    { year: 2024, month: 6, day: 12, date: '2024-06-12', title: 'Guild charter purchased' },
+    { year: 2024, month: 6, day: 13, date: '2024-06-13', title: 'Guild formed' },
+  ]
+}
+
+export default guild
