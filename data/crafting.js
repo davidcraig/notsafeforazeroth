2530082826abba2857cea13300b5bb4a8bc225eb
davidcraig <davidcraig-uk@protonmail.com>
2024-07-11 18:01:16 +0100

    Empty SL crafting temporarily.

--- data/crafting.js
+++ data/crafting.js
@@ -1,77 +1,77 @@
-import { findByName } from './characters'
-import Tailoring from './shadowlands/crafting/legendaries/tailoring'
-import Leatherworking from './shadowlands/crafting/legendaries/leatherworking'
+// import { findByName } from './characters'
+// import Tailoring from './shadowlands/crafting/legendaries/tailoring'
+// import Leatherworking from './shadowlands/crafting/legendaries/leatherworking'
 
-export const guildCrafting = {
-  tailoring: { name: 'Tailoring', crafters: [ findByName() ] },
-  blacksmithing: { name: 'Blacksmithing', crafters: [] },
-  leatherworking: { name: 'Leatherworking', crafters: [] },
+// export const guildCrafting = {
+//   tailoring: { name: 'Tailoring', crafters: [ findByName() ] },
+//   blacksmithing: { name: 'Blacksmithing', crafters: [] },
+//   leatherworking: { name: 'Leatherworking', crafters: [] },
 
-  inscription: { name: 'Inscription', crafters: [] },
-  engineering: { name: 'Engineering', crafters: [] },
+//   inscription: { name: 'Inscription', crafters: [] },
+//   engineering: { name: 'Engineering', crafters: [] },
 
-  alchemy: { name: 'Alchemy', crafters: [] },
-  jewelcrafting: { name: 'Jewelcrafting', crafters: [] },
+//   alchemy: { name: 'Alchemy', crafters: [] },
+//   jewelcrafting: { name: 'Jewelcrafting', crafters: [] },
 
-  enchanting: { name: 'Enchanting', crafters: [] },
-}
+//   enchanting: { name: 'Enchanting', crafters: [] },
+// }
 
-export const legendaries = {
-  tailoring: {
-    name: 'Tailoring',
-    crafters: [
-      {
-        crafter: findByName('Snipermagi'),
-        legendaries: [
-          { legendary: Tailoring.GRIM_VEILED_BELT, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_BRACERS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_CAPE, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_HOOD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_MITTENS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_PANTS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_SANDALS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_ROBE, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Tailoring.GRIM_VEILED_SHOULDERS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-        ]
-      }
-    ]
-  },
-  blacksmithing: {
-    name: 'Blacksmithing'
-  },
-  leatherworking: {
-    name: 'Leatherworking',
-    crafters: [
-      {
-        crafter: findByName('Tyriant'),
-        legendaries: [
-          { legendary: Leatherworking.UMBRAHIDE_WAISTGUARD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_VEST, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_TREADS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_PAULDRONS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_HELM, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_ARMGUARDS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_GAUNTLETS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.UMBRAHIDE_LEGGINGS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+// export const legendaries = {
+//   tailoring: {
+//     name: 'Tailoring',
+//     crafters: [
+//       {
+//         crafter: findByName('Snipermagi'),
+//         legendaries: [
+//           { legendary: Tailoring.GRIM_VEILED_BELT, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_BRACERS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_CAPE, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_HOOD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_MITTENS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_PANTS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_SANDALS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_ROBE, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Tailoring.GRIM_VEILED_SHOULDERS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//         ]
+//       }
+//     ]
+//   },
+//   blacksmithing: {
+//     name: 'Blacksmithing'
+//   },
+//   leatherworking: {
+//     name: 'Leatherworking',
+//     crafters: [
+//       {
+//         crafter: findByName('Tyriant'),
+//         legendaries: [
+//           { legendary: Leatherworking.UMBRAHIDE_WAISTGUARD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_VEST, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_TREADS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_PAULDRONS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_HELM, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_ARMGUARDS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_GAUNTLETS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.UMBRAHIDE_LEGGINGS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
 
-          { legendary: Leatherworking.BONESHATTER_WAISTGUARD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_VEST, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_TREADS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_PAULDRONS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_HELM, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_GREAVES, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_GAUNTLETS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-          { legendary: Leatherworking.BONESHATTER_ARMGUARDS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
-        ]
-      }
-    ]
-  },
-  jewelcrafting: {
-    name: 'Jewelcrafting'
-  }
-}
+//           { legendary: Leatherworking.BONESHATTER_WAISTGUARD, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_VEST, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_TREADS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_PAULDRONS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_HELM, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_GREAVES, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_GAUNTLETS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//           { legendary: Leatherworking.BONESHATTER_ARMGUARDS, rank: 1, progress: { rank: 1, xp: 0, xpcap: 75 } },
+//         ]
+//       }
+//     ]
+//   },
+//   jewelcrafting: {
+//     name: 'Jewelcrafting'
+//   }
+// }
 
 export default {
-  guildCrafting,
-  legendaries
+  // guildCrafting,
+  // legendaries
 }
