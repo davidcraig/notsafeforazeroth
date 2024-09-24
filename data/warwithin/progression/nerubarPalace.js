16b63176667eb5167e25ccda5bdaf6863c8c6f8e
davidcraig <davidcraig-uk@protonmail.com>
2024-09-24 09:50:25 +0100

    Update progression.

--- data/warwithin/progression/nerubarPalace.js
+++ data/warwithin/progression/nerubarPalace.js
@@ -6,27 +6,27 @@ export const nerubarPalace = [
   },
   {
     name: 'The Bloodbound Horror', slug: 'nerub-bloodbound',
-    normal: false, heroic: false, mythic: false,
+    normal: true, heroic: false, mythic: false,
     video: 'GpMXMvkiKVg', mythic_video: '', img: '2_bloodbound.jpg'
   },
   {
     name: 'Sikran, Captain of the Sureki', slug: 'nerub-sikran',
-    normal: false, heroic: false, mythic: false,
+    normal: true, heroic: false, mythic: false,
     video: 'keaSH1x2_Qs', mythic_video: '', img: '3_sikran.jpg'
   },
   {
     name: "Rasha'nan", slug: 'nerub-rasha',
-    normal: false, heroic: false, mythic: false,
+    normal: true, heroic: false, mythic: false,
     video: 'nfLMTxy5ZJA', mythic_video: '', img: '4_rashanan.png'
   },
   {
     name: "Broodtwister Ovi'nax", slug: 'nerub-ovi',
-    normal: false, heroic: false, mythic: false,
+    normal: true, heroic: false, mythic: false,
     video: '', mythic_video: '', img: '5_broodtwister.jpg'
   },
   {
     name: "Neux-Princess Ky'veza", slug: 'nerub-kyveza',
-    normal: false, heroic: false, mythic: false,
+    normal: true, heroic: false, mythic: false,
     video: 'ke32FUap15M', mythic_video: '', img: '6_kyveza.png'
   },
   {
