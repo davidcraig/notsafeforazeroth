c01bd5e2d5d2448a0aaf8182ac1365b21759cc1c
davidcraig <davidcraig-uk@protonmail.com>
2024-09-28 22:55:58 +0100

    Use click instead of hover for navbar dropdowns.

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar, Brand, Start, End, Item, Dropdown } from '@davidcraig/react-bulma'
 
 const externalLinkSvg = <svg width={20} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill='#ececec' d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/></svg>;
@@ -45,13 +45,25 @@ const wikiPages = [
   // }
 ]
 
+const DropdownLink = ({ key, title, children }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div key={key} className={`navbar-item has-dropdown is-hoverable ${open ? "is-open" : ""}`}>
+      <a className='navbar-link' onClick={(e) => { e.preventDefault; setOpen(!open) }}>{title}</a>
+      <div className='navbar-dropdown'>
+        {children}
+      </div>
+    </div>
+  )
+}
+
 function renderNavigationItem(item) {
   if (item.pages) {
-    return <Dropdown className='pr-2' key={item.name} title={item.name}>
+    return <DropdownLink className='pr-2' key={item.name} title={item.name}>
       {item.pages.map(dropdownPage => {
         return renderNavigationItem(dropdownPage)
       })}
-    </Dropdown>
+    </DropdownLink>
   }
   let itemCssClass = ''
   if (item.wowClassColour) {
