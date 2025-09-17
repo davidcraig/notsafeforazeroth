b6661a136953e565bf6a9c61d1946484195491e7
davidcraig <davidcraig1986@gmail.com>
2025-09-17 18:35:22 +0100

    Update shaman.

--- data/classes/shaman.ts
+++ data/classes/shaman.ts
@@ -2,6 +2,28 @@ import { ROLES } from '../roles'
 import type { WoWClass } from '../../Types/WoWClass'
 
 export const Shaman: WoWClass = {
-  name: 'Shaman', css: 'shaman', roles: [ ROLES.DPS, ROLES.Healer ],
+  name: 'Shaman', css: 'shaman',
+  roles: [ ROLES.DPS, ROLES.Healer ],
   img: '/img/wow/classes/shaman/shaman.png',
+  discord: ['https://discord.gg/0VcupJEQX0HuE5HH', 'https://discord.gg/AcTek6e'],
+  specs: [
+    {
+      name: 'Restoration',
+      key: 'restoration',
+      role: ROLES.Healer,
+      img: '/img/wow/classes/shaman/resto.jpg'
+    },
+    {
+      name: 'Enhancement',
+      key: 'enhancement',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/shaman/enh.jpg'
+    },
+    {
+      name: 'Elemental',
+      key: 'elemental',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/shaman/ele.jpg'
+    },
+  ]
 }
--- pages/classes/shaman.js
+++ pages/classes/shaman.js
@@ -2,25 +2,10 @@ import React from 'react'
 import Page from '../../Components/WoWClassPage'
 import { Shaman } from '../../data/classes/shaman'
 
-const guides = {
-  restoration: [],
-  enhancement: [],
-  elemental: [],
-}
-
-const DISCORD = ['https://discord.gg/0VcupJEQX0HuE5HH', 'https://discord.gg/AcTek6e']
-
 export default function ClassShaman() {
   const props = {
     title: 'Shaman',
     class: Shaman,
-    discord: DISCORD,
-    guides: guides,
-    specs: [
-      { name: 'Restoration', key: 'restoration' },
-      { name: 'Enhancement', key: 'enhancement' },
-      { name: 'Elemental', key: 'elemental' },
-    ]
   }
 
   return (
