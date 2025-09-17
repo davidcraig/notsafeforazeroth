223b2adc88ae5f379896fa94f2ccd9da2c04e0f9
davidcraig <davidcraig1986@gmail.com>
2025-09-17 18:26:40 +0100

    Add paladin changes.

--- data/classes/paladin.ts
+++ data/classes/paladin.ts
@@ -6,4 +6,25 @@ export const Paladin: WoWClass = {
   css: 'paladin',
   roles: [ ROLES.DPS, ROLES.Tank, ROLES.Healer ],
   img: '/img/wow/classes/paladin/paladin.png',
+  discord: 'https://discord.gg/hammerofwrath',
+  specs: [
+    {
+      name: 'Holy',
+      key: 'holy',
+      img: '/img/wow/classes/paladin/holy.jpg',
+      role: ROLES.Healer
+    },
+    {
+      name: 'Protection',
+      key: 'protection',
+      img: '/img/wow/classes/paladin/prot.jpg',
+      role: ROLES.Tank
+    },
+    {
+      name: 'Retribution',
+      key: 'retribution',
+      img: '/img/wow/classes/paladin/ret.jpg',
+      role: ROLES.DPS
+    },
+  ]
 }
--- pages/classes/paladin.js
+++ pages/classes/paladin.js
@@ -2,25 +2,10 @@ import React from 'react'
 import Page from '../../Components/WoWClassPage'
 import { Paladin } from '../../data/classes/paladin'
 
-const guides = {
-  holy: [],
-  protection: [],
-  retribution: [],
-}
-
-const DISCORD = 'https://discord.gg/hammerofwrath'
-
 export default function ClassPaladin() {
   const props = {
     title: 'Paladin',
     class: Paladin,
-    discord: DISCORD,
-    guides: guides,
-    specs: [
-      { name: 'Holy', key: 'holy' },
-      { name: 'Protection', key: 'protection' },
-      { name: 'Retribution', key: 'retribution' },
-    ]
   }
 
   return (
