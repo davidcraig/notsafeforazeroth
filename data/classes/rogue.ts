551bd9e0421709d1e4094b944ae0e8f712245e9e
davidcraig <davidcraig1986@gmail.com>
2025-09-17 18:32:08 +0100

    Update rogue.

--- data/classes/rogue.ts
+++ data/classes/rogue.ts
@@ -6,4 +6,25 @@ export const Rogue: WoWClass = {
   css: 'rogue',
   roles: [ ROLES.DPS ],
   img: '/img/wow/classes/rogue/rogue.png',
+  discord: 'https://discord.gg/0h08tydxoNhfDVZf',
+  specs: [
+    {
+      name: 'Outlaw',
+      key: 'outlaw',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/rogue/outlaw.jpg'
+    },
+    {
+      name: 'Subtlety',
+      key: 'subtlety',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/rogue/subt.jpg'
+    },
+    {
+      name: 'Assassination',
+      key: 'assassination',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/rogue/ass.jpg'
+    },
+  ]
 }
--- pages/classes/rogue.js
+++ pages/classes/rogue.js
@@ -2,25 +2,10 @@ import React from 'react'
 import Page from '../../Components/WoWClassPage'
 import { Rogue } from '../../data/classes/rogue'
 
-const DISCORD = 'https://discord.gg/0h08tydxoNhfDVZf'
-
-const guides = {
-  assassination: [],
-  subtlety: [],
-  outlaw: [],
-}
-
 export default function ClassRogue() {
   const props = {
     title: 'Rogue',
     class: Rogue,
-    discord: DISCORD,
-    guides: guides,
-    specs: [
-      { name: 'Outlaw', key: 'outlaw' },
-      { name: 'Subtlety', key: 'subtlety' },
-      { name: 'Assassination', key: 'assassination' },
-    ]
   }
 
   return (
