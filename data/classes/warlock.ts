6890ffde402da300e273b427be370d8606db6f79
davidcraig <davidcraig1986@gmail.com>
2025-09-17 18:38:16 +0100

    Update warlock.

--- data/classes/warlock.ts
+++ data/classes/warlock.ts
@@ -4,4 +4,25 @@ import type { WoWClass } from '../../Types/WoWClass'
 export const Warlock: WoWClass = {
   name: 'Warlock', css: 'warlock', roles: [ ROLES.DPS ],
   img: '/img/wow/classes/warlock/warlock.png',
+  discord: 'https://discord.gg/0onXDymd9Wpc2CEu',
+  specs: [
+    {
+      name: 'Affliction',
+      key: 'affliction',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/warlock/aff.jpg',
+    },
+    {
+      name: 'Destruction',
+      key: 'destruction',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/warlock/destro.jpg',
+    },
+    {
+      name: 'Demonology',
+      key: 'demonology',
+      role: ROLES.DPS,
+      img: '/img/wow/classes/warlock/demo.jpg',
+    },
+  ]
 }
--- pages/classes/warlock.js
+++ pages/classes/warlock.js
@@ -2,25 +2,10 @@ import React from 'react'
 import Page from '../../Components/WoWClassPage'
 import { Warlock } from '../../data/classes/warlock'
 
-const guides = {
-  affliction: [],
-  destruction: [],
-  demonology: [],
-}
-
-const DISCORD = 'https://discord.gg/0onXDymd9Wpc2CEu'
-
 export default function ClassWarlock() {
   const props = {
     title: 'Warlock',
     class: Warlock,
-    discord: DISCORD,
-    guides: guides,
-    specs: [
-      { name: 'Affliction', key: 'affliction' },
-      { name: 'Destruction', key: 'destruction' },
-      { name: 'Demonology', key: 'demonology' },
-    ]
   }
 
   return (
