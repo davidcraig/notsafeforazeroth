bcca808cedbb758075cc446debd2a1797e251d10
davidcraig <davidcraig1986@gmail.com>
2025-09-24 14:33:18 +0100

    Add realm comments.

--- data/characters.ts
+++ data/characters.ts
@@ -1,25 +1,30 @@
+// TarrenMill
 import Snipedeath from './characters/TarrenMill/Snipedeath'
 import Sniperdrood from './characters/TarrenMill/Sniperdrood'
 import Snipermagi from './characters/TarrenMill/Snipermagi'
-import Sniperwar from './characters/Silvermoon/Sniperwar'
-import Snipá from './characters/Magtheridon/Snipá'
-import Snipá_SM from './characters/Silvermoon/Snipá'
 import Snipevoke from './characters/TarrenMill/Snipevoke'
-import Snipepal from './characters/Silvermoon/Snipepal'
-import Aronin from './characters/TarrenMill/Aronin'
 import Snipérmonk from './characters/TarrenMill/Snipérmonk'
+import Aronin from './characters/TarrenMill/Aronin'
+// Silvermoon
+import Sniperwar_SM from './characters/Silvermoon/Sniperwar'
+import Snipá_SM from './characters/Silvermoon/Snipá'
+import Snipepal_SM from './characters/Silvermoon/Snipepal'
+// Magtheridon
+import Snipá_MAG from './characters/Magtheridon/Snipá'
 
 export const CHARACTERS = [
   Snipedeath,
   Sniperdrood,
   Snipermagi,
-  Sniperwar,
   Snipevoke,
   Snipérmonk,
-  Snipepal,
-  Snipá,
-  Snipá_SM,
   Aronin,
+  // Silvermoon
+  Sniperwar_SM,
+  Snipá_SM,
+  Snipepal_SM,
+  // Magtheridon
+  Snipá_MAG,
 ]
 
 export default CHARACTERS
