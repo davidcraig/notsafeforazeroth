7f742a3f577d2af284135e6ecb87a7f66c1edbb5
davidcraig <davidcraig1986@gmail.com>
2025-11-24 13:50:20 +0000

    Update bags page.

--- pages/guides/bags.tsx
+++ pages/guides/bags.tsx
@@ -8,13 +8,16 @@ export default function Bags() {
         <div>
           <h1 className='text-xl mb-4'>Bags</h1>
           <ul>
-            <li>[36] <a className='fg-rare' href="https://www.wowhead.com/item=222856/duskweave-bag">Duskweave Bag</a></li>
+            <li>[36] <a className='fg-rare' href="https://www.wowhead.com/item=222856/duskweave-bag">Duskweave Bag</a> - War Within Crafted</li>
             <li>[34] <a className='fg-rare' href="https://www.wowhead.com/item=202194/misty-satchel">Misty Satchel</a> - One per character, rare from Dragonflight.</li>
           </ul> 
         </div>
 
         <div>
-          <h1 className='text-xl mt-4 mb-4'>Reagent Bags</h1>
+          <h1 className='text-xl mb-4'>Reagent Bags</h1>
+          <ul>
+            <li>[32] <a className='fg-uncommon' href="https://www.wowhead.com/item=194019/simply-stitched-reagent-bag">Simply Stitched Reagent Bag</a> - Dragonflight Crafted</li>
+          </ul>
         </div>
       </div>
     </Page>
