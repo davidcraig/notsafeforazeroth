9514f94b9c477f7633c07775328b70f3ca410649
davidcraig <davidcraig1986@gmail.com>
2025-12-29 16:28:49 +0000

    Update roles

--- data/characters/TarrenMill/Crássiúss.ts
+++ data/characters/TarrenMill/Crássiúss.ts
@@ -1,10 +1,12 @@
 import type { Character } from "@davidcraig/wowdata/Character"
 import Hunter from "@davidcraig/wowdata/classes/hunter"
 import { Realm } from "@davidcraig/wowdata/Realm"
+import ROLES from "@davidcraig/wowdata/Roles"
 
 export const Crássiúss: Character = {
   name: "Crássiúss",
   realm: Realm.Kazzak,
+  role: [ ROLES.DPS ],
   wowclass: Hunter
 }
 
--- data/characters/TarrenMill/Whoorelips.ts
+++ data/characters/TarrenMill/Whoorelips.ts
@@ -1,10 +1,12 @@
 import type { Character } from "@davidcraig/wowdata/Character"
 import { Realm } from "@davidcraig/wowdata/Realm"
+import ROLES from "@davidcraig/wowdata/Roles"
 import Hunter from "@davidcraig/wowdata/classes/hunter"
 
 export const Whoorelips: Character = {
   name: "Whoorelips",
   realm: Realm.TarrenMill,
+  role: [ ROLES.DPS ],
   wowclass: Hunter
 }
 
