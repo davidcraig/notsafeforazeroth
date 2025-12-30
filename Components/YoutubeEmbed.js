064e3a8b96386ed5286e2a62583a589f45f61bad
davidcraig <davidcraig1986@gmail.com>
2025-12-30 15:20:02 +0000

    Update YouTube videos to use 16/9 aspect ratio

--- Components/YoutubeEmbed.js
+++ Components/YoutubeEmbed.js
@@ -1,11 +1,22 @@
-export default function YoutubeEmbed({ url, height = "175px" }) {
+export default function YoutubeEmbed({ url }) {
+  const containerStyle = {
+    contain: "size",
+    marginBottom: "1rem",
+    aspectRatio: "16 / 9",
+  };
+  const style = {
+    height: "100%",
+  };
+
   return (
-    <iframe
-      width="100%"
-      height={height}
-      src={`https://www.youtube.com/embed/${url}`}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen={true}
-    ></iframe>
+    <div style={containerStyle}>
+      <iframe
+        width="100%"
+        src={`https://www.youtube.com/embed/${url}`}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen={true}
+        style={style}
+      ></iframe>
+    </div>
   );
 }
--- pages/warwithin/raid/nerubarpalace.js
+++ pages/warwithin/raid/nerubarpalace.js
@@ -1,32 +1,40 @@
-import Page from '../../../Components/Page'
-import YoutubeEmbed from '../../../Components/YoutubeEmbed'
-import { default as nerubBosses } from '../../../data/warwithin/progression/nerubarPalace'
+import Page from "../../../Components/Page";
+import YoutubeEmbed from "../../../Components/YoutubeEmbed";
+import { default as nerubBosses } from "../../../data/warwithin/progression/nerubarPalace";
 
-const imgPath = '/img/warwithin/raid/nerubar/'
+const imgPath = "/img/warwithin/raid/nerubar/";
 
 export default function NerubarPalace() {
-  return <Page title='Nerub-ar Palace'>
-    <h1 className='text-xl'>Nerub-ar Palace</h1>
+  return (
+    <Page title="Nerub-ar Palace">
+      <h1 className="text-xl">Nerub-ar Palace</h1>
 
-    <h2 className='text-lg'>Boss Guides</h2>
-    <div className=''>
-
-      <div className='grid grid-cols-2 lg:grid-cols-3 gap-2 mb-2'>
-        {nerubBosses.map((boss,index) => {
-              return boss.video && (
-                <YoutubeEmbed url={boss.video} height='450' />
-              )
-        })}
+      <h2 className="text-lg">Boss Guides</h2>
+      <div className="">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 mb-2">
+          {nerubBosses.map((boss, index) => {
+            return boss.video && <YoutubeEmbed url={boss.video} />;
+          })}
         </div>
 
-      <div className='grid grid-cols-2 xl:grid-cols-3 gap-2'>
-        {nerubBosses.map((boss,index) => {
-              return boss.img && (
-                <img style={{width: '100%', flexGrow: 0, flexShrink: 0, maxWidth: '100%'}} src={boss.img ? `${imgPath}${boss.img}` : null} />
+        <div className="grid grid-cols-2 xl:grid-cols-3 gap-2">
+          {nerubBosses.map((boss, index) => {
+            return (
+              boss.img && (
+                <img
+                  style={{
+                    width: "100%",
+                    flexGrow: 0,
+                    flexShrink: 0,
+                    maxWidth: "100%",
+                  }}
+                  src={boss.img ? `${imgPath}${boss.img}` : null}
+                />
               )
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
-  </Page>
+    </Page>
+  );
 }
-
